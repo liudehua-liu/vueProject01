70b5284180561bd2ef74fcb11c3a39a9e26cf2f6
DESKTOP-J9IM5UH\lizixin <215437459@qq.com>
2019-10-10 14:36:47 +0800

    写成test_a了

---
+++ vue_work/src/components/test_a.vue
@@ -0,0 +1,16 @@
+<template>
+    <fieldset>
+        <legend><h3>test_a</h3></legend>
+        sssssssss
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "test_a"
+    }
+</script>
+
+<style scoped>
+
+</style>
