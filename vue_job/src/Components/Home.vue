f12055eed9638d6b3a8fe3c65c9319849dd5f8e3
lanou\lanou <411945330@qq.com>
2019-10-10 16:42:40 +0800

    13245235

---
+++ vue_job/src/Components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <fieldset>
+      <legend><h2>我的首页</h2></legend>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
