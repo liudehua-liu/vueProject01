e8c9d7f428f3073f0c693a5bc2cf415f5fc86368
DESKTOP-5TBM399\lx <627920201@qq.com>
2019-10-10 16:04:49 +0800

    白

---
+++ vue_job/src/Components/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+    <fieldset>
+      <legend><h2>这是cart组件</h2></legend>
+      <h1>这是cart组件</h1>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "Cart"
+    }
+</script>
+
+<style scoped>
+
+</style>
