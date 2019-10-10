629deeab0e44a0cfbaf19c97117b014916a29c37
DESKTOP-J9IM5UH\lizixin <215437459@qq.com>
2019-10-10 16:04:18 +0800

    shop

---
+++ vue_job/src/Components/Shop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+    <fieldset>
+      <legend><h3>shop</h3></legend>
+      吃的吃吃吃吃吃吃
+    </fieldset>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Shop"
+    }
+</script>
+
+<style scoped>
+
+</style>
